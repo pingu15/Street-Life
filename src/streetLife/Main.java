06248f351f40483373efa9db54e9592494bc742b
Max Sun <maxsun715@gmail.com>
2022-05-17 22:21:27 -0400

    Created Main.java

---
+++ src/streetLife/Main.java
@@ -0,0 +1,17 @@
+package streetLife;
+
+import javafx.application.*;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+	}
+
+}
