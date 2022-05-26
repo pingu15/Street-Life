18fdf9980dabf9884f4360f1f90d3150badd622e
zozoefc <zoe.fan-chiang@student.tdsb.on.ca>
2022-05-26 11:13:14 -0400

    adding class

---
+++ src/streetLife/DeficiencyRoom.java
@@ -0,0 +1,25 @@
+package streetLife;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class DeficiencyRoom implements EventHandler<ActionEvent> {
+    Stage stage;
+
+    public DeficiencyRoom(Stage s){
+        s = stage;
+    }
+
+    public void start(){}
+
+    public void handle(ActionEvent event) {
+
+    }
+}
