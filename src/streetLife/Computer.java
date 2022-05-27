320b227d1cd4e52684851b533a288bc565e15f56
Max Sun <maxsun715@gmail.com>
2022-05-27 11:35:47 -0400

    updates

--- src/streetLife/Computer.java
+++ src/streetLife/Computer.java
@@ -4,58 +4,97 @@ import java.io.IOException;
 import java.util.Arrays;
 
 import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextArea;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 
 public class Computer {
 	
-	Stage stage;
+	/**
+	 * number of choice prompts
+	 */
+	final static int CHOICES = 5;
 	
-	String name;
+	/**
+	 * name of user
+	 */
+	static String name;
 	
-	ComputerFile cur;
+	/**
+	 * reader to read from text files
+	 */
+	static Reader r;
 	
-	Reader r;
+	/**
+	 * scenes for the computer and when an image is displayed
+	 */
+	static Scene comp, image;
 	
-	Text settled;
+	/**
+	 * index of current choice
+	 */
+	static int index;
 	
-	TextArea input;
+	/**
+	 * array of choice prompts
+	 */
+	static String[] choice;
 	
-	int index;
+	/**
+	 * settled text the user cannot edit
+	 */
+	static Text settled;
 	
-	String[] choice;
+	/**
+	 * input text the user can edit
+	 */
+	static TextArea input;
 	
-	final int CHOICES = 2;
+	/**
+	 * current computer folder
+	 */
+	static ComputerFile cur;
 	
-	public Computer(Stage stage) throws IOException {
-		this.stage = stage;
+	
+	/**
+	 * back button to return to terminal screen
+	 */
+	static Button back;
+	
+	/**
+	 * Starts the panic stage using a computer terminal simulation
+	 * 
+	 * @throws IOException
+	 */
+	public static void start() throws IOException {
 		setChoices();
-	}
-	
-	public void setFile(String name, ComputerFile par) {
-		this.cur = new ComputerFile(name, par.pathName+"\\"+name, par);
-	}
-	
-	public void start() throws IOException {
 		settled = new Text();
 		settled.setText("");
 		input = new TextArea();
 		input.setText("");
 		display();
-		onEnter();
+		input();
 	}
 	
-	private void show() throws IOException {
+	/**
+	 * Creates screen with text
+	 * 
+	 * @throws IOException
+	 */
+	private static void show() throws IOException {
+		ScrollPane sp = new ScrollPane();
 		Group g = new Group();
-		Functions.setScene(stage, g, Color.BLACK);
+		g.setTranslateY(10);
 		settled.setLineSpacing(8);
 		settled.setFont(Functions.getFont("computer", 14));
 		settled.setFill(Color.WHITE);
-		settled.setWrappingWidth(Main.WIDTH-5);
+		settled.setWrappingWidth(Main.WIDTH-15);
 		settled.setTranslateX(5);
 		settled.setTranslateY(20);
 		g.getChildren().add(settled);
@@ -63,13 +102,24 @@ public class Computer {
 		input.setId("ComputerText");
 		input.setFont(Functions.getFont("computer", 14));
 		input.setWrapText(true);
-		input.setPrefWidth(Main.WIDTH-5);
-		input.setPrefHeight(Main.HEIGHT-h);
+		input.setPrefWidth(Main.WIDTH-15);
+		input.setPrefHeight(Main.HEIGHT-h-10);
 		input.setTranslateY(h);
+		input.setMaxHeight(Main.HEIGHT-h-10);
 		g.getChildren().add(input);
+		g.prefHeight(Main.HEIGHT);
+		sp.setContent(g);
+		sp.setId("ScrollPane");
+		comp = new Scene(sp, Main.WIDTH, Main.HEIGHT);
+		Functions.setScene(comp, Color.BLACK);
 	}
 	
-	private void display() throws IOException {
+	/**
+	 * Adds input text to settled text and displays it
+	 * 
+	 * @throws IOException
+	 */
+	private static void display() throws IOException {
 		String s = settled.getText();
 		if(index > 0) {
 			s += cur + "> ";
@@ -81,57 +131,120 @@ public class Computer {
 		}
 		settled.setText(s);
 		show();
-		onEnter();
 	}
 	
-	private void onEnter() {
+	/**
+	 * On user key pressed
+	 */
+	private static void input() {
 		input.setOnKeyPressed((KeyEvent e) -> {
 			if(e.getCode() == KeyCode.ENTER) {
-				if(index == 0) {
-					String s = settled.getText();
-					name = input.getText();
-					input = new TextArea();
-					s += name + "\n";
-					setFile(name, new ComputerFile(name, "C:\\Users", null));
-					settled.setText(s);
-					index++;
-					try {
-						display();
-					} catch (IOException ex) {
-						ex.printStackTrace();
-					}
-					return;
-				} 
-				if(index == 1) {
-					String s = settled.getText();
-					String v = input.getText();
-					s += v + "\n";
-					if(v.split(" ").length == 1) {
-						s += "\'" + v + "\' is not a recognized command.\n";
-					} else if(v.split(" ")[0].equals("open")) {
-						if(v.split(" ")[1].equals("bankaccount.png")) {
-							Functions.openImage(stage, "bankaccount.png");
-							index++;
-						} else {
-							s += "open: \'" + v.split(" ")[1] + "\' is not an open command.\n";
-						}
-					} else {
-						s += "\'" + v.split(" ")[0] + "\' " + Functions.getError("command") + "\n";
-					}
-					settled.setText(s);
-					input = new TextArea();
-					try {
-						display();
-					} catch (IOException ex) {
-						ex.printStackTrace();
-					}
-					return;
+				try {
+					handle();
+				} catch (IOException ex) {
+					ex.printStackTrace();
 				}
 			}
 		});
 	}
 	
-	private void setChoices() throws IOException {
+	/**
+	 * Handles input on enter key
+	 * 
+	 * @throws IOException
+	 */
+	private static void handle() throws IOException {
+		String s = settled.getText();
+		String v = input.getText();
+		s += v + "\n";
+		if(index == 0) {
+			name = v;
+			input = new TextArea();
+			input();
+			setFile(name, new ComputerFile(name, "C:\\Users", null));
+			settled.setText(s);
+			index++;
+			display();
+			return;
+		} 
+		if(index == 1) {
+			input = new TextArea();
+			boolean flag = false;
+			if(v.split(" ").length == 1) {
+				s += "\'" + v + "\' is not a recognized command.\n";
+			} else if(v.split(" ")[0].equals("open")) {
+				if(v.split(" ")[1].equals("bankaccount.png")) {
+					flag = true;
+				} else {
+					s += "open: \'" + v.split(" ")[1] + "\' is not an open command.\n";
+				}
+			} else {
+				s += "\'" + v.split(" ")[0] + "\' " + Functions.getError("command") + "\n";
+			}
+			settled.setText(s);
+			input();
+			if(flag) {
+				Functions.openImage("bankaccount.png", 72, 433, 200, 72, "backbutton");
+				onClick();
+			} else {
+				display();
+			}
+			return;
+		}
+		if(index == 2) {
+			System.out.println(v);
+			if(v.equalsIgnoreCase("y")) {
+				index = 3;
+				input = new TextArea();
+				input();
+				settled.setText(s);
+				display();
+				return;
+			}
+			if(v.equalsIgnoreCase("n")) {
+				index = 4;
+				input = new TextArea();
+				input();
+				settled.setText(s);
+				display();
+				return;
+			}
+			
+		}
+	}
+	
+	/**
+	 * Goes back to terminal screen
+	 */
+	private static void onClick() {
+		back.setOnMouseClicked((MouseEvent e) -> {
+			try {
+				handleButton();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		});
+	}
+	
+	/**
+	 * hands events based on current choice button is operating on 
+	 * 
+	 * @throws IOException
+	 */
+	private static void handleButton() throws IOException {
+		if(index == 1) {
+			index++;
+			display();
+			return;
+		}
+	}
+
+	/**
+	 * Adds the choice prompts from text file
+	 * 
+	 * @throws IOException
+	 */
+	private static void setChoices() throws IOException {
 		r = new Reader("text\\CompStart.txt");
 		choice = new String[CHOICES];
 		Arrays.fill(choice, "");
@@ -141,6 +254,15 @@ public class Computer {
 				choice[c] += r.readLine() + "\n";
 			}
 		}
+	}
+	
+	/**
+	 * 
+	 * @param name name of the folder or file
+	 * @param par name of the parent folder
+	 */
+	private static void setFile(String name, ComputerFile par) {
+		cur = new ComputerFile(name, par.pathName+"\\"+name, par);
 	}
 	
 }
--- src/streetLife/Main.java
+++ src/streetLife/Main.java
@@ -4,30 +4,47 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 /**
- * Street Life teaches users about homelessness, through a fun and interactive game
+ * Street Life is a JavaFX Application that teaches users about homelessness through an interactive game.
  * 
- * @author Max Sun, Zoe Fan-Chiang, Derek Ma
+ * @author Max Sun
+ * @author Zoe Fan-Chiang
+ * @author Derek Ma
  * @version 1.0
  * @since 2022-05-20
  */
 public class Main extends Application {
 	
-	public static final int WIDTH = 960, HEIGHT = 540;
+	/**
+	 * width of stage
+	 */
+	public static final int WIDTH = 960;
+	
+	/**
+	 * height of stage
+	 */
+	public static final int HEIGHT = 540;
+	
+	/**
+	 * primary stage
+	 */
+	public static Stage stage;
 
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
 
-	@Override
+	/**
+	 * Starts the application.
+	 * 
+	 * @param stage The primary stage on which the program runs
+	 * @Override
+	 */
 	public void start(Stage stage) throws Exception {
 		Functions.onLoad();
+		Main.stage = stage;
+		stage.setResizable(false);
 		stage.setTitle("Street Life");
-		Intro intro = new Intro(stage);
-		intro.start();
-		Computer comp = new Computer(stage);
-		comp.start();
-		//EscapeRoom escape = new EscapeRoom(stage);
-		//escape.start();
+		Computer.start();
 	}
 
 }
