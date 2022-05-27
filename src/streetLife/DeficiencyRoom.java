e290ddcecf5b605733aeb52332ac42dcba29e4be
zozoefc <zoe.fan-chiang@student.tdsb.on.ca>
2022-05-27 12:26:23 -0400

    coding deficiency room

--- src/streetLife/DeficiencyRoom.java
+++ src/streetLife/DeficiencyRoom.java
@@ -13,13 +13,54 @@ import javafx.stage.Stage;
 public class DeficiencyRoom implements EventHandler<ActionEvent> {
     Stage stage;
 
+    private ImageView screen1 = new ImageView();
+    private Button button1;
+    private Pane pane1 = new Pane();
+    private Group root1 = new Group();
+    private Scene scene1 = new Scene(root1, 960, 540);
+
+    private ImageView screen2 = new ImageView();
+    private Pane pane2 = new Pane();
+    private Group root2 = new Group();
+    private Scene scene2 = new Scene(root2, 960, 540);
+
     public DeficiencyRoom(Stage s){
-        s = stage;
+        stage = s;
     }
 
-    public void start(){}
+    public void start(){
+        stage.setTitle("Deficiency Room");
+        stage.setX(10);
+        stage.setY(10);
+
+        button1 = new Button();
+        button1.setText("continue");
+        button1.setOnAction(this);
+        button1.setLayoutX(850);
+        button1.setLayoutY(490);
+        pane1.getChildren().addAll(button1);
+
+        Image image1 = new Image("deficiency 1.png");
+        screen1.setImage(image1);
+        screen1.setFitWidth(960);
+        screen1.setPreserveRatio(true);
+
+        root1.getChildren().addAll(screen1, pane1);
+
+        Image image2 = new Image("deficiency 2.png");
+        screen2.setImage(image2);
+        screen2.setFitWidth(960);
+        screen2.setPreserveRatio(true);
+
+        root2.getChildren().addAll(screen2, pane2);
+
+        stage.setScene(scene1);
+        stage.show();
+    }
 
     public void handle(ActionEvent event) {
-
+        if(event.getSource() == button1){
+            stage.setScene(scene2);
+        }
     }
 }
