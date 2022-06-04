b03cb6152bf3ca6e0de02383d4e389e72a88aee5
zozoefc <zoe.fan-chiang@student.tdsb.on.ca>
2022-06-03 23:59:47 -0400

    comment

--- src/streetLife/DeficiencyRoom.java
+++ src/streetLife/DeficiencyRoom.java
@@ -8,19 +8,18 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import javafx.scene.input.*;
+import java.time.LocalTime;
 
 public class DeficiencyRoom implements EventHandler<ActionEvent> {
     Stage stage;
 
     //scene 1 variables
     private ImageView screen1 = new ImageView();
-    private Button button1;
     private Pane pane1 = new Pane();
     private Group root1 = new Group();
     private Scene scene1 = new Scene(root1, 960, 540);
@@ -59,6 +58,31 @@ public class DeficiencyRoom implements EventHandler<ActionEvent> {
     private Group root4c = new Group();
     private Scene scene4c = new Scene(root4c, 960, 540);
 
+    private ImageView screen4d = new ImageView();
+    private Pane pane4d = new Pane();
+    private Group root4d = new Group();
+    private Scene scene4d = new Scene(root4d, 960, 540);
+
+    private ImageView screen4e = new ImageView();
+    private Pane pane4e = new Pane();
+    private Group root4e = new Group();
+    private Scene scene4e = new Scene(root4e, 960, 540);
+
+    private ImageView screen4f = new ImageView();
+    private Pane pane4f = new Pane();
+    private Group root4f = new Group();
+    private Scene scene4f = new Scene(root4f, 960, 540);
+
+    private ImageView screen4g = new ImageView();
+    private Pane pane4g = new Pane();
+    private Group root4g = new Group();
+    private Scene scene4g = new Scene(root4g, 960, 540);
+
+    private ImageView screen4h = new ImageView();
+    private Pane pane4h = new Pane();
+    private Group root4h = new Group();
+    private Scene scene4h = new Scene(root4h, 960, 540);
+
     /**
      * constructor
      * @param s stage which room should run on
@@ -68,7 +92,7 @@ public class DeficiencyRoom implements EventHandler<ActionEvent> {
     }
 
     /**
-     * setting up each scene and running it
+     * setting up each scene and running the first scene
      */
     public void start(){
         //general
@@ -77,13 +101,6 @@ public class DeficiencyRoom implements EventHandler<ActionEvent> {
         stage.setY(10);
 
         //scene 1
-        button1 = new Button();
-        button1.setText("continue");
-        button1.setOnAction(this);
-        button1.setLayoutX(850);
-        button1.setLayoutY(490);
-        pane1.getChildren().addAll(button1);
-
         Image image1 = new Image("deficiency 1.png");
         screen1.setImage(image1);
         screen1.setFitWidth(960);
@@ -136,22 +153,67 @@ public class DeficiencyRoom implements EventHandler<ActionEvent> {
 
         root4c.getChildren().addAll(screen4c, pane4c);
 
+        Image image4d = new Image("deficiency 4 pop up (4).png");
+        screen4d.setImage(image4d);
+        screen4d.setFitWidth(960);
+        screen4d.setPreserveRatio(true);
+
+        root4d.getChildren().addAll(screen4d, pane4d);
+
+        Image image4e = new Image("deficiency 4 pop up (5).png");
+        screen4e.setImage(image4e);
+        screen4e.setFitWidth(960);
+        screen4e.setPreserveRatio(true);
+
+        root4e.getChildren().addAll(screen4e, pane4e);
+
+        Image image4f = new Image("deficiency 4 pop up (6).png");
+        screen4f.setImage(image4f);
+        screen4f.setFitWidth(960);
+        screen4f.setPreserveRatio(true);
+
+        root4f.getChildren().addAll(screen4f, pane4c);
+
+        Image image4g = new Image("deficiency 4 pop up (7).png");
+        screen4g.setImage(image4g);
+        screen4g.setFitWidth(960);
+        screen4g.setPreserveRatio(true);
+
+        root4g.getChildren().addAll(screen4g, pane4g);
+
+        Image image4h = new Image("deficiency 4 pop up (8).png");
+        screen4h.setImage(image4h);
+        screen4h.setFitWidth(960);
+        screen4h.setPreserveRatio(true);
+
+        root4h.getChildren().addAll(screen4h, pane4h);
+
         //start
+        int start = LocalTime.now().getSecond();
+        if(start >= 54) {
+            start -= 60;
+        }
+
+        //trying to get timer two work so that scene switches after certain amount of time but it shows blank screen
         stage.setScene(scene1);
         stage.show();
-    }
 
-    /**
-     * handle method for buttons and key presses, etc.
-     * @param event the event which occurred
-     */
-    public void handle(ActionEvent event) {
-        if(event.getSource() == button1){
-            stage.setScene(scene2);
-            inputS2();
+        while(true){
+            if(LocalTime.now().getSecond() == start + 6){
+                stage.setScene(scene2);
+                inputS2();
+                break;
+            }
         }
     }
 
+    public void handle(ActionEvent e){
+
+    }
+
+    /**
+     * method to deal with input on screen 2 which calls screen 3
+     */
     public void inputS2(){
         scene2.setOnKeyPressed((KeyEvent e) -> {
             if(e.getCode() == KeyCode.SPACE){
@@ -161,6 +223,9 @@ public class DeficiencyRoom implements EventHandler<ActionEvent> {
         });
     }
 
+    /**
+     * method to deal with input on screen 3 which calls screen 4
+     */
        public void inputS3(){
         scene3.setOnMouseClicked((MouseEvent e) -> {
             if(e.getX() > 150 && e.getX() < 260 && e.getY() > 350 && e.getY() < 380){
@@ -170,6 +235,9 @@ public class DeficiencyRoom implements EventHandler<ActionEvent> {
         });
     }
 
+    /**
+     * method to deal with input on screen 4 which calls pop up screens
+     */
     public void inputS4(){
         scene4.setOnMouseClicked((MouseEvent e) -> {
             if(e.getX() > 700 && e.getX() < 850 && e.getY() > 365 && e.getY() < 490){
@@ -184,9 +252,33 @@ public class DeficiencyRoom implements EventHandler<ActionEvent> {
                 stage.setScene(scene4c);
                 inputPopUpS4(scene4c);
             }
+            if(e.getX() > 100 && e.getX() < 250 && e.getY() > 365 && e.getY() < 490){
+                stage.setScene(scene4d);
+                inputPopUpS4(scene4d);
+            }
+            if(e.getX() > 700 && e.getX() < 850 && e.getY() > 60 && e.getY() < 175){
+                stage.setScene(scene4e);
+                inputPopUpS4(scene4e);
+            }
+            if(e.getX() > 500 && e.getX() < 650 && e.getY() > 60 && e.getY() < 175){
+                stage.setScene(scene4f);
+                inputPopUpS4(scene4f);
+            }
+            if(e.getX() > 300 && e.getX() < 450 && e.getY() > 60 && e.getY() < 175){
+                stage.setScene(scene4g);
+                inputPopUpS4(scene4g);
+            }
+            if(e.getX() > 100 && e.getX() < 250 && e.getY() > 60 && e.getY() < 175){
+                stage.setScene(scene4h);
+                inputPopUpS4(scene4h);
+            }
         });
     }
 
+    /**
+     * method for the input on pop up screens which calls back to screen 4
+     * @param s
+     */
     public void inputPopUpS4(Scene s){
         s.setOnKeyPressed((KeyEvent e) -> {
             if(e.getCode() == KeyCode.SPACE){
